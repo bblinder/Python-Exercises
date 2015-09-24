cdacbf9b802666d113596a04bf1cda032237788b
bblnder <brandon@BlindySupport.local>
2015-09-24 10:05:24 -0400

    Added check for requests module

--- ISPcheck.py
+++ ISPcheck.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-import requests
-import re
+import sys
+try:
+	import requests
+except ImportError:
+	print "Please install the 'requests' module."
+	print "This can be done by running 'sudo -H pip install requests'"
+	sys.exit()
 
 data = requests.get('http://icanhazip.com')
 ISP = data.text
